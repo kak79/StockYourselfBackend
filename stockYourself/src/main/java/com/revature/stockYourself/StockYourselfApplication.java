3aa79152091499f2e8ec3425e494c27b492a49f8
unknown <brett.dixon@revature.net>
2022-01-12 10:26:12 -0700

    back-end start

---
+++ stockYourself/src/main/java/com/revature/stockYourself/StockYourselfApplication.java
@@ -0,0 +1,13 @@
+package com.revature.stockYourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockYourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockYourselfApplication.class, args);
+	}
+
+}
---
+++ stockYourself/src/test/java/com/revature/stockYourself/StockYourselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.stockYourself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockYourselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
