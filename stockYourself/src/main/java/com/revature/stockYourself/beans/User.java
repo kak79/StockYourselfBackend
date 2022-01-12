f58b2ea424659c54597b732166f39538899f4412
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-01-12 16:03:18 -0600

    DDL.sql, User and Portfolio beans

---
+++ stockYourself/src/main/java/com/revature/stockYourself/beans/User.java
@@ -0,0 +1,30 @@
+package com.revature.stockYourself.beans;
+
+public class User {
+
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String passwrd;
+	private String email;
+	private String phoneNumber;
+	private Role role;
+	private Portfolio portfolio;
+	
+	public User() {
+		userId = 0;
+		firstName = "First";
+		lastName = "Last";
+		username = "username";
+		passwrd = "pass";
+		email = "first@123.com";
+		phoneNumber = "(123)456-7890";
+		role = new Role();
+		portfolio = new Portfolio();
+	}
+	
+	
+	
+	
+}
