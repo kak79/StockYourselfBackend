848d33aaae3a6f092b85698223c94dd8b6cc18f0
unknown <brett.dixon@revature.net>
2022-01-18 20:52:16 -0700

    ddl dml

--- database/DDL.sql
+++ database/DDL.sql
@@ -1,35 +1,38 @@
-drop table if exists user;
-drop table if exists user_role;
-drop table if exists portfolio;
-
-CREATE table user (
-user_id serial PRIMARY KEY,
-username varchar(40)unique not null,
-passwrd varchar(40) not null,
-email varchar(70) unique not null,
-phone_number integer unique,
-role_id integer REFERENCES role not null,
-portfolio_id integer REFERENCES portfolio
-);
+drop table if exists user_role cascade;
+drop table if exists stock_string cascade;
+drop table if exists portfolio cascade;
+drop table if exists user_table cascade;
+drop table if exists stock_portfolio cascade;
 
 CREATE table user_role (
 	role_id serial PRIMARY KEY,
 	role_name varchar(40)
 );
 
+CREATE TABLE stock_string (
+stock_string_id serial PRIMARY KEY,
+stock_name varchar(6)
+);
+
 CREATE table portfolio (
 portfolio_id serial PRIMARY key
-stock1 varchar(5),
-stock2 varchar(5),
-stock3 varchar(5),
-stock4 varchar(5),
-stock5 varchar(5)
+);
+
+CREATE table user_table (
+user_id serial PRIMARY KEY,
+username varchar(40)unique not null,
+passwrd varchar(40) not null,
+email varchar(70) unique not null,
+phone_number integer unique,
+role_id integer REFERENCES user_role not null,
+portfolio_id integer REFERENCES portfolio
 );
 
 
-create table stock_owner (
-	portfolio_id integer references portfolio,
-	stock_id varchar(5)
-);
 
+
+create table  stock_portfolio(
+	portfolio_id integer REFERENCES portfolio,
+	stock_string_id integer REFERENCES stock_string
+);
 
