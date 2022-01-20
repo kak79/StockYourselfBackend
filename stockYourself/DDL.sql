3ce8ed186cecb6a8be5dffab9ae6881bffab3484
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-01-20 10:18:18 -0600

    Add 3 columns to the DB, portfolio_name to portfolio, post_title and portfolio_id to post.

--- stockYourself/DDL.sql
+++ stockYourself/DDL.sql
@@ -1,41 +1,46 @@
-DROP table IF EXISTS user;
-DROP table IF EXISTS user_role;
-DROP table IF EXISTS portfolio;
-DROP table IF EXISTS stock_portfolio;
-DROP table IF EXISTS post;
 
-CREATE table user (
-user_id serial PRIMARY KEY,
+
+DROP table IF EXISTS post;
+DROP table IF EXISTS stock_portfolio;
+DROP table IF EXISTS us_r;
+DROP table IF EXISTS portfolio;
+DROP table IF EXISTS user_role;
+
+
+CREATE table IF NOT EXISTS user_role (
+	role_id serial UNIQUE PRIMARY KEY,
+	role_name varchar(40)
+);
+
+CREATE table IF NOT EXISTS portfolio (
+portfolio_id serial UNIQUE PRIMARY key,
+portfolio_name varchar(10)
+);
+
+CREATE table IF NOT EXISTS us_r (
+user_id serial UNIQUE PRIMARY KEY,
 first_name varchar(40) NOT NULL,
 last_name varchar(40) NOT NULL,
 username varchar(40) UNIQUE NOT NULL,
 passwrd varchar(40) NOT NULL,
 email varchar(70) UNIQUE NOT NULL,
 phone_number varchar(15),
-role_id integer REFERENCES role NOT NULL,
+role_id integer REFERENCES user_role NOT NULL,
 portfolio_id integer REFERENCES portfolio
 );
 
-CREATE table user_role (
-	role_id serial PRIMARY KEY,
-	role_name varchar(40)
-);
 
-CREATE table portfolio (
-portfolio_id serial PRIMARY KEY
-);
-
-
-CREATE table stock_portfolio (
+CREATE table IF NOT EXISTS stock_portfolio (
 	portfolio_id integer REFERENCES portfolio,
 	stock_id varchar(5)
 );
 
-CREATE table post (
-	post_id serial PRIMARY KEY,
+CREATE table IF NOT EXISTS post (
+	post_id serial UNIQUE PRIMARY KEY,
+	post_title varchar(50),
 	post_content varchar(200),
 	creation_date DATE NOT NULL,
 	creation_time TIME NOT NULL,
-	user_id integer REFERENCES user
+	user_id integer REFERENCES us_r,
+	portfolio_id integer REFERENCES portfolio
 );
-
